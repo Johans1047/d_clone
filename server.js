c680af2d66cb86f5692431c0c8c634a42d04177b
Diego Chan <diego.chan@utp.ac.pa>
2023-12-19 22:18:32 -0500

    Create user model

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-// ENTRY POINT
+const express = require("express");
+const UserController = require("./server/controllers/UserController").UserController;
+const app = express();
+const PORT = 3306;
+const HOST = "127.0.0.1";
+
+userCont = new UserController();
+
+app.get("/", (req, res) => {
+    res.send("<h1>HELLO WORLD</h1>\n<br>");
+});
+
+app.get("/users/login", (req, res) => {
+    res.send(userCont.login());
+});
+
+app.get("/users/signup", (req, res) => {
+    res.send(userCont.signup());
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}!\nClick here: http://${HOST}:${PORT}`);
+});
